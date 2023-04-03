5b0b9c6155eb2d69309fb97c55eb8c3f9b21da10
Adalberto Santos <adalbertoliveira@gmail.com>
2023-04-03 15:40:53 -0300

    Initial project created.

---
+++ src/main/java/com/ontop/webapi/webapiontop/WebApiOntopApplication.java
@@ -0,0 +1,13 @@
+package com.ontop.webapi.webapiontop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiOntopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApiOntopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ontop/webapi/webapiontop/WebApiOntopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontop.webapi.webapiontop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiOntopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
