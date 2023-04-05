fc2621c4fc2e7c8f7d9238d98e2123b85ed1a2d4
Adalberto Santos <adalbertoliveira@gmail.com>
2023-04-05 18:48:05 -0300

    Create the database connection and initial tables.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+create table if not exists Account (
+    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(255),
+    surname varchar(255),
+    routingNumber number(9),
+    nationalId varchar(255),
+    accountNumber number(8),
+    bankName varchar(255)
+);
+
+create table if not exists Transaction (
+   user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   name varchar(255),
+   surname varchar(255),
+   routingNumber number(9),
+   nationalId varchar(255),
+   accountNumber number(8),
+   bankName varchar(255)
+);
