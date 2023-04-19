8bfd6048a8d4c872ed45f5c563d3c573dfd2be40
Adalberto Santos <adalbertoliveira@gmail.com>
2023-04-19 15:44:17 -0300

    Start using JUnit and Mockito.

--- src/test/java/com/ontop/webapi/webapiontop/WebApiOntopApplicationTests.java
+++ src/test/java/com/ontop/webapi/webapiontop/WebApiOntopApplicationTests.java
@@ -6,6 +6,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebApiOntopApplicationTests {
 
+
+    /*
+    https://www.springboottutorial.com/unit-testing-for-spring-boot-rest-services
+
+
+     */
+
+    /* TODO: Finish this application using the JUNIT and MOCKITO. */
+
+
     @Test
     void contextLoads() {
     }
